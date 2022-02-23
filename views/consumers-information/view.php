d0a6aee9ad47c914ef207a51305ee0477e03be73
Damirbek <damirbek@gmail.com>
2022-02-23 18:11:38 +0600

    title issue

--- views/consumers-information/view.php
+++ views/consumers-information/view.php
@@ -7,11 +7,11 @@ use yii\widgets\DetailView;
 /* @var $model app\models\Consumer */
 
 $this->title = $title;
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $model->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="consumer-view">
-<h1 class="main-heading"><?= Html::encode($this->title) ?></h1>    
+<h1 class="main-heading"><?= Html::encode($model->title) ?></h1>    
     <div class="row top-margin-20">
         <div class="col-lg-12"><?= $model->text; ?></div>
     </div>
