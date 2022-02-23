5f4f9bd5b20e4a379094d455fb3ee63ceb37c51f
Damirbek <damirbek@gmail.com>
2022-02-23 18:16:06 +0600

    title issue

--- views/businesses-information/view.php
+++ views/businesses-information/view.php
@@ -6,14 +6,14 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Business */
 
-$this->title = $model->title;
+$this->title = $title;
 $this->params['breadcrumbs'][] = Yii::t('app', 'Businesses');
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $model->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="business-view">
     <h1 class="main-heading">
-        <?= Html::encode($this->title) ?>
+        <?= Html::encode($model->title) ?>
     </h1>
     <div class="row top-margin-20">
         <div class="col-lg-12"><?= $model->text; ?></div>
