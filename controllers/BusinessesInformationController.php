1af8675d31419bdb090e51ef5eac0823655af318
Damirbek <damirbek@gmail.com>
2022-02-23 18:14:58 +0600

    title issue

--- controllers/BusinessesInformationController.php
+++ controllers/BusinessesInformationController.php
@@ -39,11 +39,12 @@ class BusinessesInformationController extends Controller
      */
     public function actionIndex($type)
     {
-        App::registerSeoTags();
+        $title = App::registerSeoStatic();
         $model = $this->findModel($type);
         $model->translate(Yii::$app->language);
         return $this->render('view', [
             'model' => $model,
+            'title'=>$title,
         ]);
     }
 
