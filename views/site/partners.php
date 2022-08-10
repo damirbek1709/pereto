ec489a136aa537eda0a150970ba2ae3574115fa4
damirbek1709 <damirbek@gmail.com>
2022-08-10 12:18:04 +0600

    css upgrades

--- views/site/partners.php
+++ views/site/partners.php
@@ -12,22 +12,22 @@ use yii\helpers\Url;
             <div class="row">
                 <div class="col-lg-3 col-md-6">
                     <div class="site-partner-block">
-                        <?= Html::a(Html::img(Url::base() . '/images/partners/auca.png'), 'https://www.auca.kg/', ['target' => '_blank']); ?>
+                        <?= Html::a(Html::img(Url::base() . '/images/partner/auca.png'), 'https://www.auca.kg/', ['target' => '_blank']); ?>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <div class="site-partner-block">
-                        <?= Html::a(Html::img(Url::base() . '/images/partners/unison.png'), 'https://unisongroup.org/', ['target' => '_blank']); ?>
+                        <?= Html::a(Html::img(Url::base() . '/images/partner/unison.png'), 'https://unisongroup.org/', ['target' => '_blank']); ?>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <div class="site-partner-block">
-                        <?= Html::a(Html::img(Url::base() . '/images/partners/technopolis.png'), 'https://www.technopolis-group.com/', ['target' => '_blank']); ?>
+                        <?= Html::a(Html::img(Url::base() . '/images/partner/technopolis.png'), 'https://www.technopolis-group.com/', ['target' => '_blank']); ?>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <div class="site-partner-block">
-                        <?= Html::a(Html::img(Url::base() . '/images/partners/cscp.png'), 'https://www.cscp.org/', ['target' => '_blank']); ?>
+                        <?= Html::a(Html::img(Url::base() . '/images/partner/cscp.png'), 'https://www.cscp.org/', ['target' => '_blank']); ?>
                     </div>
                 </div>
             </div>
