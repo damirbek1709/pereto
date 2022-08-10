f0209ee992dec35f7356f9ae7c7273d25d48a947
damirbek1709 <damirbek@gmail.com>
2022-08-10 12:16:39 +0600

    css upgrades

--- views/site/partners.php
+++ views/site/partners.php
@@ -12,12 +12,12 @@ use yii\helpers\Url;
             <div class="row">
                 <div class="col-lg-3 col-md-6">
                     <div class="site-partner-block">
-                        <?= Html::a(Html::img(Url::base() . '/images/partners/american_u.png'), 'https://www.auca.kg/', ['target' => '_blank']); ?>
+                        <?= Html::a(Html::img(Url::base() . '/images/partners/auca.png'), 'https://www.auca.kg/', ['target' => '_blank']); ?>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <div class="site-partner-block">
-                        <?= Html::a(Html::img(Url::base() . '/images/partners/unison_logo.png'), 'https://unisongroup.org/', ['target' => '_blank']); ?>
+                        <?= Html::a(Html::img(Url::base() . '/images/partners/unison.png'), 'https://unisongroup.org/', ['target' => '_blank']); ?>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6">
